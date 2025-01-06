3172297ccc1225bd201dd7a8d71f75c574581e08
roudressh <rhbuddy1@gmail.com>
2025-01-06 20:17:31 +0530

    hello

--- oops_concepts/src/day_1_class_and_objects/Class_Creation.java
+++ oops_concepts/src/day_1_class_and_objects/Class_Creation.java
@@ -4,7 +4,7 @@ public class Class_Creation {
 int x=5;//declaring the variable
 	public static void main(String[] args) {
 		Class_Creation n = new Class_Creation();//creating an object
-		n.x=10; //modifying attributes
+		//n.x=10; //modifying attributes
 		System.out.println(n.x); //printing the value
 //accessing attributes using (.) syntax
 	}
